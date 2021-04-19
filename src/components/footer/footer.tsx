f01a8e0462c647961664222b9bcf1376f51e5bf7
tangling <tangling@zwlbs.com>
2021-04-19 17:57:30 +0800

    111

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,16 +6,47 @@ import { Link } from 'umi'
 import st from './footer.less'
 const Header = () => {
   return (
-    <div className={st.header}>
+    <div className={st.footer}>
       <div className={st.box}>
-        <div className={st.logo}>logo</div>
-        <Menu
-          defaultSelectedKeys={['1']}
-          mode="horizontal"
-          theme="dark"
-        >
-          <div>asds</div>
-        </Menu>
+        <div>
+          <div>产品技术</div>
+          <ul>
+            <li>产品技术1</li>
+            <li>产品技术2</li>
+            <li>产品技术3</li>
+          </ul>
+        </div>
+        <div>
+          <div>解决方案</div>
+          <ul>
+            <li>行业应用1</li>
+            <li>行业应用2</li>
+          </ul>
+        </div>
+        <div>
+          <div>技术支持</div>
+          <ul>
+            <li>开发者中心</li>
+            <li>技术协议</li>
+          </ul>
+        </div>
+        <div>
+          <div>商务合作</div>
+          <ul>
+            <li>技术咨询</li>
+            <li>应用案例</li>
+            <li>资源合作</li>
+          </ul>
+        </div>
+        <div>
+          <div>关于THESEUS</div>
+          <ul>
+            <li>企业文化</li>
+            <li>发展规划</li>
+            <li>信息资讯</li>
+            <li>加入我们</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
